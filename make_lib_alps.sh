e0685e511984a3f8df1f6b76f1fd66da10dcaebd
aroccon <alessio.roccon@uniud.it>
2026-02-17 13:38:48 +0100

    alps_25_7_ok

--- make_lib_alps.sh
+++ make_lib_alps.sh
@@ -2,13 +2,15 @@
 #uenv 25.7 does not work, but 24.11 does work, so we use that one
 #uenv image find prgenv-nvfortran
 #uenv image pull prgenv-nvfortran/24.11:v2 
-#uenv start prgenv-nvfortran/24.11:v2 --view=nvfort,modules
-
+uenv start --view=nvfort prgenv-nvfortran/25.7:v2
 
 git clone https://github.com/NVIDIA/cuDecomp
 cd cuDecomp
 mkdir build
 cd build
+
+#needed when using 25.7 
+export CMAKE_PREFIX_PATH=/user-environment/linux-neoverse_v2/nvhpc-25.7-rcus56e4ntq7bzgxbw6vrdi6celzcpgz/Linux_aarch64/25.7:$CMAKE_PREFIX_PATH
 
 cmake ..
 make -j
