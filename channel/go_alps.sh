ef965793f480bd7801b6c161b7265525bd4861a9
aroccon <alessio.roccon@uniud.it>
2026-02-20 13:43:24 +0100

    alps_no_Nccl

--- channel/go_alps.sh
+++ channel/go_alps.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #SBATCH --account=lp146
 #SBATCH --job-name=example-%j
-#SBATCH --time=00:05:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --gpus-per-task=1
+#SBATCH --time=00:5:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=4               # total ranks
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=4
+#SBATCH --ntasks=8
+
 CURRENT_DIR="$(pwd)"
 ROOT_DIR="$(dirname "$CURRENT_DIR")/cuDecomp/build/lib"
 echo "Using directory: $ROOT_DIR"
 export LD_LIBRARY_PATH=$ROOT_DIR:$LD_LIBRARY_PATH
 
-srun -n4 mhit36
+srun  mhit36
